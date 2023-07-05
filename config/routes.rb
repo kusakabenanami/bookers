024f59d763ea887ba5934cc9e9086eb1467062bc
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-05 09:12:14 +0000

    [Add]3-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'books/top'
+  post 'books' => 'books#create'
+  get 'homes/top'
+  root to: 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
