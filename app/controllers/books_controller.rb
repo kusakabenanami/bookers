e81e3225837c01eb648c8b7dfb9c05e0fb32c5ba
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-09 14:14:01 +0000

    [Add]3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,23 +15,30 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(@books.id)
+    redirect_to book_path(book.id)
   end
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book= Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
+    if @book.save
     # 4. リダイレクト
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path
+    if book.destroy  # データ（レコード）を削除
+      flash[:notice] = "Title can't be blank" "Body can't be blank"
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   private
